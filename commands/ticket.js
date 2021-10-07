c2f583ed858ffad819fbbabd7d919b0ba217024f
FirephoenixX02 <otis.schmedt@gmx.de>
2021-10-07 13:45:09 +0200

    New Ticket System

--- commands/ticket.js
+++ commands/ticket.js
@@ -1,50 +1,61 @@
-const channelID = "882599174225420338";
-const check = "✅";
-let registered = false;
-
 module.exports = {
   name: "ticket",
-  description: "Creates a Support Ticket.",
-  minArgs: 1,
-  expectedArgs: "<msg>",
-  execute(userMsg, args, text) {
-    const registerEvent = (client) => {
-      if (registered) {
-        return;
+  description: "Create a Support Ticket",
+  async execute(msg, args, cmd, client, discord) {
+    const channel = await msg.guild.channels.create(
+      `ticket: ${msg.author.tag}`
+    );
+
+    channel.updateOverwrite(msg.guild.id, {
+      SEND_MESSAGE: false,
+      VIEW_CHANNEL: false,
+    });
+    channel.updateOverwrite(msg.author, {
+      SEND_MESSAGE: true,
+      VIEW_CHANNEL: true,
+    });
+
+    const reactionMessage = await channel.send(
+      "Thank you for contacting our support team!"
+    );
+
+    try {
+      await reactionMessage.react("🔒");
+      await reactionMessage.react("🗑️");
+    } catch (err) {
+      channel.send("Error Sending Emojis");
+      throw err;
+    }
+
+    const collector = reactionMessage.createReactionCollector(
+      (reaction, user) =>
+        msg.guild.members.cache
+          .find((member) => member.id === user.id)
+          .hasPermission("ADMINISTRATOR"),
+      { dispose: true }
+    );
+
+    collector.on("collect", (reaction, user) => {
+      switch (reaction.emoji.name) {
+        case "🔒":
+          channel.updateOverwrite(msg.author, {
+            SEND_MESSAGE: false,
+          });
+          break;
+        case "🗑️":
+          channel.send("This channel will be deleted in 5s!");
+          setTimeout(() => channel.delete(), 5000);
+          break;
       }
-
-      registered = true;
-
-      console.log("Registering Event");
-
-      client.on("message", (reaction, user) => {
-        console.log("Handling Reaction");
-        const { msg } = reaction;
-        if (msg.channel.id === channelID) {
-          msg.delete();
-        }
-      });
-    };  
-    const Discord = require("discord.js");
-    const client = new Discord.Client();
-    const { guild, member } = userMsg;
-
-    registerEvent(client);
-
-    const channel = guild.channels.cache.get(channelID);
-    channel
-      .send(
-        `A new Support Ticket has been created by <@${member.id}>
-        "${text}"
-
-Click the ${check} icon then this ticket has been resolved`
-
-      )
-      .then((ticketMsg) => {
-        ticketMsg.react(check);
-
-        userMsg.reply("Your ticket has been created! Expect a reply in 24h.");
-        userMsg.delete();
+    });
+    msg.channel
+      .send(`We will be right with you! ${channel}`)
+      .then((message) => {
+        setTimeout(() => message.delete(), 7000);
+        setTimeout(() => msg.delete(), 3000);
+      })
+      .catch((err) => {
+        throw err;
       });
   },
 };
