1cffd4991a00cdc7a2e1c729e8de28793b2f715d
FirephoenixX02 <otis.schmedt@gmx.de>
2021-10-18 15:35:31 +0200

    Clear Cmd Perm Fix

--- commands/clear.js
+++ commands/clear.js
@@ -3,15 +3,17 @@ module.exports = {
   description: "Clear messages from the channel.",
   args: true,
   usage: "<number greater than 0, less than 100>",
-  execute(msg, args) {
+  async execute(msg, args) {
     const amount = parseInt(args[0]) + 1;
+    const user = msg.author;
+    const userMember = await msg.guild.members.fetch(user)
 
     if (isNaN(amount)) {
       return msg.reply("that doesn't seem to be a valid number.");
     } else if (amount <= 1 || amount > 100) {
       return msg.reply("you need to input a number between 1 and 99.");
     }
-    if (msg.author.hasPermission("ADMINISTRATOR")) {
+    if (userMember.hasPermission("ADMINISTRATOR")) {
       msg.channel.bulkDelete(amount, true).catch((err) => {
         console.error(err);
         msg.channel.send(
