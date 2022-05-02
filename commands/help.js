06c886b240a819336b94c8f7fa145d58997918ae
FirephoenixX02 <otis.schmedt@gmx.de>
2022-05-02 19:07:09 +0200

    Removed autpublish and added dynamic help menu

--- commands/help.js
+++ commands/help.js
@@ -1,33 +1,61 @@
+const Discord = require("discord.js");
+const BOT_PREFIX = ",";
+
 module.exports = {
   name: "help",
-  description: "Shows all Commands",
-  execute(msg) {
-    const Discord = require("discord.js");
+  description: "Gives a list of all commands",
+  async execute(msg, args, client) {
+    const data = [];
+    const { commands } = client;
+
+    if (!args.length) {
+      data.push(`List of all commands: \n`);
+      data.push(commands.map((c) => c.name).join(`\n${BOT_PREFIX}`)
+      );
+      data.push(
+        "\nTo get more info about a specific command, do " +
+          BOT_PREFIX +
+          "help [command name]"
+      );
+      const embed = new Discord.MessageEmbed()
+        .setColor("RED")
+        .setTitle("All Fire Bot Commands")
+        .setDescription(data.toString())
+        .setFooter({ text: "Bot made by NieGestorben#6618" });
+
+      msg.channel.send({ embeds: [embed] });
+
+      return;
+    }
+
+    const name = args[0];
+    const cmd =
+      commands.get(name) ||
+      commands.find((c) => c.aliases && c.aliases.includes(name));
+
+    if (!cmd) {
+      msg.channel.send(`Command \`${name}\` not found.`);
+
+      return;
+    }
+
+    data.push(`Name: ${cmd.name}`);
+
+    if (cmd.description) {
+      data.push(`Description: ${cmd.description}`);
+    }
+    if (cmd.aliases) {
+      data.push(`Aliases: ${cmd.aliases})`);
+    }
+
     const embed = new Discord.MessageEmbed()
       .setColor("RED")
-      .setTitle("All Fire Bot Commands")
-      .setDescription(
-        ",clear Number - Deletes a number of messages in a channel\n" +
-          ",coinflip - Flips a coin\n" +
-          ",dice NUMBER - Rolls a dice with the amount of faces you want\n" +
-          ",help - Shows all Commands\n" +
-          ",invites - Shows how many new user you invited to the server\n" +
-          ",news - Gives you the Announcement Role \n" +
-          ",ping - Command used for testing the Calculation time of the bot\n" +
-          ",serverinfo - Gives Info about the Server your on.\n" +
-          ",botinfo - Gives you Information about the Bot\n" +
-          ",play + Link or Keywords - Plays Music\n" +
-          ",airhorn - Plays an Airhorn Sound in the voice channel your in\n" +
-          ",userinfo (@User) - Gives Info about a specific User\n" +
-          ",mcserver IP PORT(Often 25565) - Get the Status of a MC Server\n" +
-          ",hypixelbw IGN - Shows Hypixel BW Stats from a Player\n" +
-          ",math QUESTION - Calculates a Math equation\n" +
-          ",gitstatus - Get the status of the git repository\n" +
-          ",imdb MOVIE- Gives info about a movie via OMDb(The Open Movie Database)\n" +
-          ""
-      )
+      .setTitle("Command Info")
+      .setDescription(data.toString())
       .setFooter({ text: "Bot made by NieGestorben#6618" });
 
     msg.channel.send({ embeds: [embed] });
+
+    return;
   },
 };
